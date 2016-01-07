a232aefbc9a4989ccb9f0a7ffbed3f9f1b5d846e
Kelley Crowell <1kelley.crowell@gmail.com>
2016-01-07 05:26:08 +0000

    Added mailer and views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,7 +16,7 @@ class ContactsController < ApplicationController
     flash[:danger] = 'Error occured, message has not been sent.'
     redirect_to new_contact_path
   end
-end
+  end
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
