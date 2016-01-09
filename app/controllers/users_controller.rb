86b911f29a940ccd082b44925e95e39f537520b7
Kelley Crowell <1kelley.crowell@gmail.com>
2016-01-09 02:32:32 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
-    beofre_action :authenticate_user!
+    before_action :authenticate_user!
     def show
         @user = User.find(params[:id])
+    end
+    
+    def index
     end
 end
